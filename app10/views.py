8a9b063025eae0aa2e2cb9a9c5875b955330fe30
Roopasree19 <roopasreek19@gmail.com>
2023-02-24 11:51:35 +0530

    rs

--- app10/views.py
+++ app10/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app10.models import *
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -28,8 +29,12 @@ def services(request):
 
 #############################################################################
 def admin_index(request):
-	return render(request,'admin/index.html')
+	if request.session.has_key("my_id"):
+		return render(request,'admin/index.html')
+	else:
+		return HttpResponseRedirect('/admin_login')
 
+		
 def admin_register(request):
 	if request.method == "POST":
 		cemail=request.POST['email']
@@ -37,13 +42,32 @@ def admin_register(request):
 		cpass=request.POST['confirmpassword']
 		check=reg_tb.objects.filter(email=cemail)
 		if check:
-			return render(request,'admin/register.html',{'error':'already registered'})
+			return render(request,'admin/register.html',{'error':'Already registered'})
 		else:
 			add=reg_tb(email=cemail,password=cpassword,conformpassword=cpass)
 			add.save()
-		return render(request,'admin/index.html',{'success':"data saved"})
+		return render(request,'admin/index.html',{'success':"successfully registered"})
 	else:
 	    return render(request,'admin/register.html')
 
 def admin_login(request):
-	return render(request,'admin/login.html')
+	if request.method == "POST":
+		cemail=request.POST['email']
+		cpassword=request.POST['password']
+		check=reg_tb.objects.filter(email=cemail,password=cpassword)
+		if check:
+			for x in check:
+				request.session['id']=x.id
+				request.session['email']=x.email
+			return render(request,'admin/index.html',{'success':'successfully logined'})
+		else:
+			return render(request,'admin/login.html',{'error':' invalid details'})
+	else:
+		return render(request,'admin/login.html')
+
+
+def admin_logout(request):
+	if request.session.has_key("id"):
+	    del request.session['id']
+	    del request.session['email']
+	return HttpResponseRedirect ('/admin_login/')
